a8257aa858142654b2a6b43b3e812ef1e4e23f62
will.l.y.wang <971183242@qq.com>
2020-07-23 23:01:05 +0800

    init

---
+++ src/main/java/com/example/knight/KnightApplication.java
@@ -0,0 +1,13 @@
+package com.example.knight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnightApplication.class, args);
+    }
+
+}
